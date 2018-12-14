3628b3e860b095317082594dff24c9658125da49
Huang Zhibo <zhibohuang@creditease.cn>
2018-12-14 10:39:00 +0800

    代码备份

---
+++ src/main/java/com/mjolnir/datastructure/list/skiplist/SkipList.java
@@ -0,0 +1,136 @@
+package com.mjolnir.datastructure.list.skiplist;
+
+import java.util.Random;
+
+/**
+ * 跳表
+ * @author huangzhibo
+ * @date 2018/12/12
+ */
+public class SkipList {
+
+    private static final int MAX_LEVEL = 16;
+
+    private int levelCount = 1;
+
+    private Node head = new Node();
+
+    private Random r = new Random();
+
+    public Node find(int value){
+        Node p = head;
+        for (int i = levelCount - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value){
+                p = p.forwards[i];
+            }
+        }
+
+        if (p.forwards[0] != null && p.forwards[0].data == value){
+            return p.forwards[0];
+        } else {
+            return null;
+        }
+    }
+
+    public void insert(int value){
+        int level = randomLevel();
+        Node newNode = new Node();
+        newNode.data = value;
+        newNode.maxLevel = level;
+        Node[] update = new Node[level];
+        for (int i = 0; i < level; i++) {
+            update[i] = head;
+        }
+
+        Node p = head;
+        for (int i = level-1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value){
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        for (int i = 0; i < level; i++) {
+            newNode.forwards[i] = update[i].forwards[i];
+            update[i].forwards[i] = newNode;
+        }
+
+        if (levelCount < level){
+            levelCount = level;
+        }
+    }
+
+    public void delete(int value){
+        Node[] update = new Node[levelCount];
+        Node p = head;
+        for (int i = levelCount-1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value){
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        if (p.forwards[0] != null && p.forwards[0].data == value){
+            for (int i = levelCount-1; i >= 1; --i) {
+                if (update[i].forwards[i] != null && update[i].forwards[i].data == value){
+                    update[i].forwards[i] = update[i].forwards[i].forwards[i];
+                }
+            }
+        }
+    }
+
+    // 随机 level 次，如果是奇数层数 +1，防止伪随机
+    private int randomLevel(){
+        int level = 1;
+        for (int i = 1; i < MAX_LEVEL; i++) {
+            if (r.nextInt() % 2 == 1){
+                level++;
+            }
+        }
+        return level;
+    }
+
+    public void printAll() {
+        Node p = head;
+        while (p.forwards[0] != null) {
+            System.out.print(p.forwards[0] + " ");
+            p = p.forwards[0];
+        }
+        System.out.println();
+    }
+
+    class Node {
+
+        private int data = -1;
+        private Node[] forwards = new Node[MAX_LEVEL];
+        private int maxLevel = 0;
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{ data: ");
+            builder.append(data);
+            builder.append("; levels: ");
+            builder.append(maxLevel);
+            builder.append(" }");
+            return builder.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        SkipList skipList = new SkipList();
+        skipList.insert(1);
+        skipList.insert(2);
+        skipList.insert(12);
+        skipList.insert(23);
+        skipList.insert(5);
+        skipList.insert(7);
+        skipList.insert(90);
+        skipList.insert(145);
+        skipList.insert(9);
+        skipList.insert(9);
+        System.out.println(skipList.head);
+        System.out.println(skipList.find(90));
+    }
+
+}
